57dda296685059e948a10968954a283038073ab8
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2023-01-03 15:01:16 +0700

    Create Image Atom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
-import { Text, Container } from "./components/atoms";
+import { Text, Container, Image } from "./components/atoms";
 
 function App() {
   return (
     <div className="App">
       <Container width="80%" margin="auto">
         <Text textType="b2">Hello World</Text>
+        <Image
+          width="100%"
+          src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+        />
       </Container>
     </div>
   );
