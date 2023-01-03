49d8ba229190b6beb8c7e0de506a2a70e922153b
Muhammad Naufal <45099597+verm024@users.noreply.github.com>
2023-01-03 15:38:30 +0700

    Task / 5 / Create Button Atom (#5)

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as Text } from "./Text";
 export { default as Container } from "./Container";
 export { default as Image } from "./Image";
+export { default as Button } from "./Button";
