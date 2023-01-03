ea2326ec6832b2ebda5752a046a3c72bc6675577
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2023-01-04 00:53:55 +0700

    Create Input Atom

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -3,3 +3,4 @@ export { default as Container } from "./Container";
 export { default as Image } from "./Image";
 export { default as Button } from "./Button";
 export { default as Spacer } from "./Spacer";
+export { default as Input } from "./Input";
