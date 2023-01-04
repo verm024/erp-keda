7f0731d58b5f54d50bfdbe067242f751e63f9164
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2023-01-04 16:59:41 +0700

    Responsive Navbar

--- src/components/molecules/Navbar/Navbar.tsx
+++ src/components/molecules/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styled from "styled-components";
 
 import { useNavigate, useLocation } from "react-router-dom";
 
 import { Container, Text, Button } from "../../atoms";
+import { useResponsive } from "../../../hooks";
 
 const NavLink = styled.a`
   cursor: pointer;
@@ -19,10 +20,29 @@ const StyledButton = styled(Button)`
   margin-left: 24px;
 `;
 
+const ResponsiveDropdownContainer = styled(Container)`
+  z-index: 1000;
+  background: white;
+  margin-top: 100px;
+  position: absolute;
+  right: 20px;
+  top: -82px;
+  border-radius: 8px;
+  box-shadow: 11px 10px 36px -11px rgba(0, 0, 0, 0.36);
+  -webkit-box-shadow: 11px 10px 36px -11px rgba(0, 0, 0, 0.36);
+  -moz-box-shadow: 11px 10px 36px -11px rgba(0, 0, 0, 0.36);
+`;
+
+const ResponsiveNavLink = styled(NavLink)`
+  padding: 8px 32px;
+`;
+
 const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const isHomePage = location.pathname === "/";
+  const { isMobile, isTablet } = useResponsive();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <Container
@@ -42,26 +62,60 @@ const Navbar = () => {
           HOME
         </Text>
       </NavLink>
-      <Container display="flex" alignItems="center">
-        <ExtendedNavLink href="#about">
-          <Text textType="h6" bold>
-            ABOUT
-          </Text>
-        </ExtendedNavLink>
-        <ExtendedNavLink href="#pricing">
-          <Text textType="h6" bold>
-            PRICING
-          </Text>
-        </ExtendedNavLink>
-        <ExtendedNavLink href="#contact">
-          <Text textType="h6" bold>
-            CONTACT
-          </Text>
-        </ExtendedNavLink>
-        <StyledButton variant="secondary" onClick={() => navigate("/login")}>
-          Login
-        </StyledButton>
-      </Container>
+      {!(isMobile || isTablet) ? (
+        <Container display="flex" alignItems="center">
+          <ExtendedNavLink href="#about">
+            <Text textType="h6" bold>
+              ABOUT
+            </Text>
+          </ExtendedNavLink>
+          <ExtendedNavLink href="#pricing">
+            <Text textType="h6" bold>
+              PRICING
+            </Text>
+          </ExtendedNavLink>
+          <ExtendedNavLink href="#contact">
+            <Text textType="h6" bold>
+              CONTACT
+            </Text>
+          </ExtendedNavLink>
+          <StyledButton variant="secondary" onClick={() => navigate("/login")}>
+            Login
+          </StyledButton>
+        </Container>
+      ) : (
+        <Container onClick={() => setIsMenuOpen(true)} padding="0 12px">
+          <Text textType="h4">=</Text>
+        </Container>
+      )}
+      {(isMobile || isTablet) && isMenuOpen && (
+        <ResponsiveDropdownContainer
+          display="flex"
+          flexDirection="column"
+          padding="8px 0"
+        >
+          <Container
+            width="100%"
+            display="flex"
+            justifyContent="space-between"
+            padding="0 12px"
+          >
+            <div />
+            <Container onClick={() => setIsMenuOpen(false)}>
+              <Text textType="h6">X</Text>
+            </Container>
+          </Container>
+          <ResponsiveNavLink href="#about">
+            <Text textType="b2">About</Text>
+          </ResponsiveNavLink>
+          <ResponsiveNavLink href="#pricing">
+            <Text textType="b2">Pricing</Text>
+          </ResponsiveNavLink>
+          <ResponsiveNavLink href="#contact">
+            <Text textType="b2">Contact</Text>
+          </ResponsiveNavLink>
+        </ResponsiveDropdownContainer>
+      )}
     </Container>
   );
 };
